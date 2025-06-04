2a304fd2e736e4280b95784784b30a0be0d8ef52
Ry4nGP <ryan.paschoalin11@gmail.com>
2025-06-03 21:50:43 -0300

    isso ai

--- Memoria.c
+++ Memoria.c
@@ -2,24 +2,22 @@
 #include <stdlib.h>
 #include <time.h>
 #include <windows.h> // Para Sleep e system("cls")
-#include <iostream>
 
 #define TAM 4
 #define TOTAL_PARES 8
 
-void gerarMatriz(int matriz[TAM][TAM])
-{
+// Função para gerar a matriz com pares embaralhados
+void gerarMatriz(int matriz[TAM][TAM]) {
     int pares[16];
     int usados[10] = {0};
     int count = 0;
 
-    srand(time(NULL));
+    srand(time(NULL)); // Garante números diferentes a cada execução
 
-    while (count < TOTAL_PARES)
-    {
+    // Gera 8 pares de números aleatórios de 0 a 9, sem repetir mais de 2 vezes
+    while (count < TOTAL_PARES) {
         int num = rand() % 10;
-        if (usados[num] < 2)
-        {
+        if (usados[num] < 2) {
             pares[count * 2] = num;
             pares[count * 2 + 1] = num;
             usados[num] += 2;
@@ -27,165 +25,160 @@ void gerarMatriz(int matriz[TAM][TAM])
         }
     }
 
-    // Embaralhar
-    for (int i = 15; i > 0; i--)
-    {
+    // Embaralhar os pares
+    for (int i = 15; i > 0; i--) {
         int j = rand() % (i + 1);
         int temp = pares[i];
         pares[i] = pares[j];
         pares[j] = temp;
     }
 
-    // Preencher matriz
+    // Preencher a matriz 4x4 com os pares embaralhados
     int k = 0;
-    for (int i = 0; i < TAM; i++)
-        for (int j = 0; j < TAM; j++)
+    for (int i = 0; i < TAM; i++) {
+        for (int j = 0; j < TAM; j++) {
             matriz[i][j] = pares[k++];
-}
-
-void mostrarMatrizNumeros(int matriz[TAM][TAM])
-{
-    printf("\n    0   1   2   3\n");
-    printf("  +---+---+---+---+\n");
-    for (int i = 0; i < TAM; i++)
-    {
-        printf("%d |", i);
-        for (int j = 0; j < TAM; j++)
-        {
-            printf(" %d |", matriz[i][j]);
         }
-        printf("\n  +---+---+---+---+\n");
     }
-    Sleep(5000); // 5 segundos
-    system("cls");
 }
 
-void mostrarMatrizVisivel(char visivel[TAM][TAM])
-{
+// Função para mostrar a matriz visível (com os números descobertos ou escondidos)
+void mostrarMatrizVisivel(char visivel[TAM][TAM]) {
     printf("\n    0   1   2   3\n");
     printf("  +---+---+---+---+\n");
-    for (int i = 0; i < TAM; i++)
-    {
+    for (int i = 0; i < TAM; i++) {
         printf("%d |", i);
-        for (int j = 0; j < TAM; j++)
-        {
+        for (int j = 0; j < TAM; j++) {
             printf(" %c |", visivel[i][j]);
         }
         printf("\n  +---+---+---+---+\n");
     }
 }
 
-int coordenadaValida(int l, int c)
-{
+// Função para mostrar os números reais da matriz (por 5 segundos)
+void mostrarMatrizNumeros(int matriz[TAM][TAM]) {
+    printf("\n    0   1   2   3\n");
+    printf("  +---+---+---+---+\n");
+    for (int i = 0; i < TAM; i++) {
+        printf("%d |", i);
+        for (int j = 0; j < TAM; j++) {
+            printf(" %d |", matriz[i][j]);
+        }
+        printf("\n  +---+---+---+---+\n");
+    }
+    Sleep(5000); // Aguarda 5 segundos
+    system("cls"); // Limpa a tela
+}
+
+// Verifica se a coordenada digitada é válida
+int coordenadaValida(int l, int c) {
     return l >= 0 && l < TAM && c >= 0 && c < TAM;
 }
 
-int jogarRodada(int matriz[TAM][TAM], char visivel[TAM][TAM], int *acertos, int *erros)
-{
+// Função para executar uma rodada do jogo
+int jogarRodada(int matriz[TAM][TAM], char visivel[TAM][TAM], int *acertos, int *erros) {
     int l1, c1, l2, c2;
 
-    mostrarMatrizVisivel(visivel);
+    mostrarMatrizVisivel(visivel); // Mostra a matriz atual
 
-    do
-    {
+    // Entrada da 1ª posição
+    do {
         printf("\nDigite linha e coluna da 1ª posição (0 a 3): ");
         scanf("%d %d", &l1, &c1);
     } while (!coordenadaValida(l1, c1));
 
     printf("Valor na posição (%d,%d): %d\n", l1, c1, matriz[l1][c1]);
 
-    do
-    {
+    // Entrada da 2ª posição
+    do {
         printf("Digite linha e coluna da 2ª posição (0 a 3): ");
         scanf("%d %d", &l2, &c2);
     } while (!coordenadaValida(l2, c2));
 
     printf("Valor na posição (%d,%d): %d\n", l2, c2, matriz[l2][c2]);
 
-    if (matriz[l1][c1] == matriz[l2][c2] && (l1 != l2 || c1 != c2))
-    {
+    // Verifica se formou um par válido
+    if (matriz[l1][c1] == matriz[l2][c2] && (l1 != l2 || c1 != c2)) {
         visivel[l1][c1] = matriz[l1][c1] + '0';
         visivel[l2][c2] = matriz[l2][c2] + '0';
         (*acertos)++;
-        putchar(7); // beep
+        putchar(7); // Beep do sistema
         printf("Par encontrado! Acertos: %d\n", *acertos);
-    }
-    else
-    {
+    } else {
         printf("Errado!\n");
         (*erros)++;
-        Sleep(2000); // 2 segundos
+        Sleep(2000); // Espera 2 segundos
     }
 
+    // Termina se acertou todos os pares ou errou mais de 10 vezes
     return *acertos == 8 || *erros > 10;
 }
 
-void salvarPontuacao(int acertos, int erros)
-{
+// Função para salvar a pontuação no arquivo
+void salvarPontuacao(int acertos, int erros) {
     char nome[50];
     char salvar;
 
     printf("Deseja salvar sua pontuação? (s/n): ");
     scanf(" %c", &salvar);
 
-    if (salvar == 's' || salvar == 'S')
-    {
+    if (salvar == 's' || salvar == 'S') {
         printf("Digite seu nome: ");
         scanf("%s", nome);
 
         FILE *arquivo = fopen("jogomemoria2025.txt", "a");
-        if (arquivo)
-        {
+        if (arquivo) {
             fprintf(arquivo, "Nome: %s | Acertos: %d | Erros: %d\n", nome, acertos, erros);
             fclose(arquivo);
             printf("Pontuação salva!\n");
-        }
-        else
-        {
+        } else {
             printf("Erro ao salvar a pontuação.\n");
         }
     }
 }
 
-void exibirMenu()
-{
-    std::cout << R"(
-     _    _     _          _                        ____                      
-    / \  | |___| |__   ___(_)_ __ ___   ___ _ __   / ___| __ _ _ __ ___   ___ 
-   / _ \ | |_  / '_ \ / _ \ | '_ ` _ \ / _ \ '__| | |  _ / _` | '_ ` _ \ / _ \
-  / ___ \| |/ /| | | |  __/ | | | | | |  __/ |    | |_| | (_| | | | | | |  __/
- /_/   \_\_/___|_| |_|\___|_|_| |_| |_|\___|_|     \____|\__,_|_| |_| |_|\___|
-                                                                              
-                                em C++
-    )" << std::endl;
+// Função para exibir o menu principal
+void exibirMenu() {
+    char simbolo1 = 16; // Caractere ASCII (Seta para direita)
+    char simbolo2 = 17; // Caractere ASCII (Seta para esquerda)
 
-    std::cout << "\n==== MENU PRINCIPAL ====\n";
-    std::cout << "1. Iniciar\n";
-    std::cout << "2. Sair\n";
-    std::cout << "Escolha uma opcao: ";
+    printf(
+        "\n"
+        "     _    _     _          _                        ____                      \n"
+        "    / \\  | |___| |__   ___(_)_ __ ___   ___ _ __   / ___| __ _ _ __ ___   ___ \n"
+        "   / _ \\ | |_  / '_ \\ / _ \\ | '_ ` _ \\ / _ \\ '__| | |  _ / _` | '_ ` _ \\ / _ \\\n"
+        "  / ___ \\| |/ /| | | |  __/ | | | | | |  __/ |    | |_| | (_| | | | | | |  __/\n"
+        " /_/   \\_\\_/___|_| |_|\\___|_|_| |_| |_|\\___|_|     \\____|\\__,_|_| |_| |_|\\___|\n"
+        "                                                                              \n"
+        "                                \n"
+    );
+
+    printf("\n MENU PRINCIPAL\n");
+    printf("%c 1 Iniciar Jogo\n", simbolo1);
+    printf("%c 2 Sair\n", simbolo2);
+    printf("\nEscolha uma opção: ");
 }
 
-void iniciarJogo()
-{
+// Função principal do jogo
+void iniciarJogo() {
     int matriz[TAM][TAM];
     char visivel[TAM][TAM];
-    char simbolo = '*';
+    char simbolo = 3; // Caractere ASCII (coração)
     char jogarNovamente;
 
-    do
-    {
+    do {
         int acertos = 0, erros = 0;
 
         gerarMatriz(matriz);
         mostrarMatrizNumeros(matriz);
 
+        // Preenche a matriz visível com símbolos
         for (int i = 0; i < TAM; i++)
             for (int j = 0; j < TAM; j++)
                 visivel[i][j] = simbolo;
 
         int fim = 0;
-        while (!fim)
-        {
+        while (!fim) {
             fim = jogarRodada(matriz, visivel, &acertos, &erros);
         }
 
@@ -199,31 +192,29 @@ void iniciarJogo()
     } while (jogarNovamente == 's' || jogarNovamente == 'S');
 }
 
-int main()
-{
+// Função principal do programa
+int main() {
     int opcao;
 
-    do
-    {
+    do {
         exibirMenu();
-        std::cin >> opcao;
+        scanf("%d", &opcao);
 
-        switch (opcao)
-        {
+        switch (opcao) {
             case 1:
                 iniciarJogo();
                 break;
             case 2:
-                std::cout << "\nSaindo...\n";
+                printf("\nSaindo...\n");
                 break;
             default:
-                std::cout << "\nOpcao invalida. Tente novamente.\n";
+                printf("\nOpcao invalida. Tente novamente.\n");
                 Sleep(1500);
                 system("cls");
         }
 
     } while (opcao != 2);
 
-    printf("Obrigado por jogar!\n");
+    printf("Saiu!\n");
     return 0;
 }
---
+++ esqueleto.cpp
@@ -0,0 +1,145 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <time.h>
+#include <windows.h> // Para Sleep e system("cls")
+
+#define TAM 4
+#define TOTAL_PARES 8
+
+void gerarMatriz(int matriz[TAM][TAM]) {
+    int pares[16];
+    int usados[10] = {0};
+    int count = 0;
+
+    srand(time(NULL));
+
+    while (count < TOTAL_PARES) {
+        int num = rand() % 10;
+        if (usados[num] < 2) {
+            pares[count * 2] = num;
+            pares[count * 2 + 1] = num;
+            usados[num] += 2;
+            count++;
+        }
+    }
+
+    // Embaralhar
+    for (int i = 15; i > 0; i--) {
+        int j = rand() % (i + 1);
+        int temp = pares[i];
+        pares[i] = pares[j];
+        pares[j] = temp;
+    }
+
+    // Preencher matriz
+    int k = 0;
+    for (int i = 0; i < TAM; i++)
+        for (int j = 0; j < TAM; j++)
+            matriz[i][j] = pares[k++];
+}
+
+void mostrarMatrizNumeros(int matriz[TAM][TAM]) {
+    printf("\nMatriz de números (memorize em 5 segundos):\n");
+    for (int i = 0; i < TAM; i++) {
+        for (int j = 0; j < TAM; j++) {
+            printf(" %d ", matriz[i][j]);
+        }
+        printf("\n");
+    }
+    Sleep(5000); // 5 segundos
+    system("cls");
+}
+
+void mostrarMatrizVisivel(char visivel[TAM][TAM]) {
+    printf("\nMatriz do jogo:\n");
+    for (int i = 0; i < TAM; i++) {
+        for (int j = 0; j < TAM; j++) {
+            printf(" %c ", visivel[i][j]);
+        }
+        printf("\n");
+    }
+}
+
+int jogarRodada(int matriz[TAM][TAM], char visivel[TAM][TAM], int* acertos, int* erros) {
+    int l1, c1, l2, c2;
+
+    mostrarMatrizVisivel(visivel);
+
+    printf("\nDigite linha e coluna da 1ª posição (0 a 3): ");
+    scanf("%d %d", &l1, &c1);
+    printf("Valor: %d\n", matriz[l1][c1]);
+
+    printf("Digite linha e coluna da 2ª posição (0 a 3): ");
+    scanf("%d %d", &l2, &c2);
+    printf("Valor: %d\n", matriz[l2][c2]);
+
+    if (matriz[l1][c1] == matriz[l2][c2] && (l1 != l2 || c1 != c2)) {
+        visivel[l1][c1] = matriz[l1][c1] + '0';
+        visivel[l2][c2] = matriz[l2][c2] + '0';
+        (*acertos)++;
+        putchar(7); // som (beep)
+        printf("Par encontrado! Acertos: %d\n", *acertos);
+    } else {
+        printf("Errado!\n");
+        (*erros)++;
+        Sleep(2000); // 2 segundos
+    }
+
+    return *acertos == 8 || *erros > 10;
+}
+
+void salvarPontuacao(int acertos, int erros) {
+    char nome[50];
+    char salvar;
+
+    printf("Deseja salvar sua pontuação? (s/n): ");
+    scanf(" %c", &salvar);
+
+    if (salvar == 's' || salvar == 'S') {
+        printf("Digite seu nome: ");
+        scanf("%s", nome);
+
+        FILE* arquivo = fopen("jogomemoria2025.txt", "a");
+        if (arquivo) {
+            fprintf(arquivo, "Nome: %s | Acertos: %d | Erros: %d\n", nome, acertos, erros);
+            fclose(arquivo);
+            printf("Pontuação salva!\n");
+        } else {
+            printf("Erro ao salvar a pontuação.\n");
+        }
+    }
+}
+
+int main() {
+    int matriz[TAM][TAM];
+    char visivel[TAM][TAM];
+    char jogarNovamente;
+    char simbolo = 5;
+
+    do {
+        int acertos = 0, erros = 0;
+
+        gerarMatriz(matriz);
+        mostrarMatrizNumeros(matriz);
+
+        for (int i = 0; i < TAM; i++)
+            for (int j = 0; j < TAM; j++)
+                visivel[i][j] = simbolo;
+
+        int fim = 0;
+        while (!fim) {
+            fim = jogarRodada(matriz, visivel, &acertos, &erros);
+        }
+
+        printf("\nFim do jogo. Acertos: %d | Erros: %d\n", acertos, erros);
+        salvarPontuacao(acertos, erros);
+
+        printf("Deseja jogar novamente? (s/n): ");
+        scanf(" %c", &jogarNovamente);
+        system("cls");
+
+    } while (jogarNovamente == 's' || jogarNovamente == 'S');
+
+    printf("Obrigado por jogar!\n");
+    return 0;
+}
